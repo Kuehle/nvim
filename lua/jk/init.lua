02443e8d3271a10c2776a5cdcec7e4fd405ed5df
Johannes Kühlewindt <johannes.kuehlewindt@gmail.com>
2023-02-17 11:48:26 +0100

    include search repository

--- lua/jk/init.lua
+++ lua/jk/init.lua
@@ -2,6 +2,19 @@ print("JK loaded")
 
 local set = vim.opt -- set options
 
+vim.opt.laststatus = 3
+vim.opt.relativenumber = true
+
+-- yiW yanks all the text between white space
+-- C-u C-d quickly move through the pages
+-- relative line numbers
+-- =ap - format all the paragraph
+-- o transfers cursor to either side of the highlighting mode
+-- this allows you to Vi{ to select everything in the body of a function and then
+-- just extend the selection to include the function header
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+
 set.tabstop = 4
 set.softtabstop = 4
 set.shiftwidth = 4
@@ -10,26 +23,28 @@ set.relativenumber = true
 set.scrolloff = 8
 
 vim.g.netrw_banner = false
-vim.g.netrw_liststyle=3
+vim.g.netrw_liststyle = 3
+--
+-- vim.keymap.set("i", "\"", "\"\"<left>", { remap = false, silent = true })
+-- vim.keymap.set("i", "'", "''<left>", { remap = false, silent = true })
+-- vim.keymap.set("i", "(", "()<left>", { remap = false, silent = true })
+-- vim.keymap.set("i", "[", "[]<left>", { remap = false, silent = true })
+-- vim.keymap.set("i", "{", "{}<left>", { remap = false, silent = true })
+-- vim.keymap.set("i", "{<CR>", "{<CR>}<ESC>O", { remap = false, silent = true })
+-- vim.keymap.set("i", "{;<CR>", "{<CR>};<ESC>O", { remap = false, silent = true })
+--
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { silent = true })
+vim.keymap.set("n", "<leader>bn", ":bNext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { silent = true })
 
-vim.keymap.set("i", "\"", "\"\"<left>", {remap = false, silent = true})
-vim.keymap.set("i", "'", "''<left>", {remap = false, silent = true})
-vim.keymap.set("i", "(", "()<left>", {remap = false, silent = true})
-vim.keymap.set("i", "[", "[]<left>", {remap = false, silent = true})
-vim.keymap.set("i", "{", "{}<left>", {remap = false, silent = true})
-vim.keymap.set("i", "{<CR>", "{<CR>}<ESC>O", {remap = false, silent = true})
-vim.keymap.set("i", "{;<CR>", "{<CR>};<ESC>O", {remap = false, silent = true})
+vim.keymap.set("n", "<leader>ee", ":Ex<CR>", { silent = true })
+vim.keymap.set("n", "<leader>eh", ":Sex<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ev", ":Vex<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", {silent = true})
-vim.keymap.set("n", "<leader>bn", ":bNext<CR>", {silent = true})
-vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", {silent = true})
-vim.keymap.set("n", "<leader>ee", ":Ex<CR>", {silent = true})
-vim.keymap.set("n", "<leader>eh", ":Sex<CR>", {silent = true})
-vim.keymap.set("n", "<leader>ev", ":Vex<CR>", {silent = true})
 vim.keymap.set("n", "<leader>tt", ":term ")
 
 vim.o.background = "light"
 
-vim.api.nvim_create_user_command('Jktest', function ()
-  print("hello from jk test")
+vim.api.nvim_create_user_command('Jktest', function()
+    print("hello from jk test")
 end, {})
