46f602bbc13005f5199d90ec921c77854ba456dc
Johannes Kuehlewindt <johannes@grover.com>
2024-07-19 19:27:35 +0200

    rust clippy

--- init.lua
+++ init.lua
@@ -347,11 +347,14 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  rust_analyzer = {
+    ["rust-analyzer"] = {
+      checkOnSave = {
+        command = "clippy",
+      },
+    },
+  },
+  tsserver = {},
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
